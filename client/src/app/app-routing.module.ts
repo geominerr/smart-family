69180cf1a264bd2027473b8f3a4716beddc65e5f
Maksym Voloshyn <mv.webdew@gmail.com>
2024-04-13 23:04:09 +0300

    feat: add auth routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'signin',
+    loadComponent: () =>
+      import('./auth/components/signin/signin.component').then(
+        (m) => m.SigninComponent
+      ),
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./auth/components/signup/signup.component').then(
+        (m) => m.SignupComponent
+      ),
+  },
+  {
+    path: 'password-recovery',
+    loadComponent: () =>
+      import(
+        './auth/components/password-recovery/password-recovery.component'
+      ).then((m) => m.PasswordRecoveryComponent),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
