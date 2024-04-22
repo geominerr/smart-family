fa4dcbe0c3b137894f15ea6d948128f1a3e18f7f
Maksym Voloshyn <mv.webdew@gmail.com>
2024-04-22 09:30:10 +0300

    fix: change app routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from '@app/auth/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadComponent: () =>
-      import('./dashboard/componets/dashboard/dashboard.component').then(
-        (m) => m.DashboardComponent
-      ),
-    canActivate: [AuthGuard],
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
   },
 ];
 
