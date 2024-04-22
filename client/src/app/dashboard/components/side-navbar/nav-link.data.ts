6e4c540a7b242335ace356b87bd468f8eae0d38d
Maksym Voloshyn <mv.webdew@gmail.com>
2024-04-22 09:32:42 +0300

    feat: add side-navbar

---
+++ client/src/app/dashboard/components/side-navbar/nav-link.data.ts
@@ -0,0 +1,43 @@
+export const navLinks = [
+  {
+    icon: 'overview',
+    routerLink: 'overview',
+    content: 'Overview',
+  },
+  {
+    icon: 'transaction',
+    routerLink: 'transactions',
+    content: 'Transactions',
+  },
+  {
+    icon: 'wallet',
+    routerLink: 'income',
+    content: 'Income',
+  },
+
+  {
+    icon: 'expenses',
+    routerLink: 'expenses',
+    content: 'Expenses',
+  },
+  {
+    icon: 'bill',
+    routerLink: 'bills',
+    content: 'Bills',
+  },
+  {
+    icon: 'goal',
+    routerLink: 'goals',
+    content: 'Goals',
+  },
+  {
+    icon: 'settings',
+    routerLink: 'settings',
+    content: 'Settings',
+  },
+  {
+    icon: 'logout',
+    routerLink: 'logout',
+    content: 'Logout',
+  },
+];
---
+++ client/src/app/dashboard/models/nav-link.model.ts
@@ -0,0 +1,5 @@
+export interface INavLink {
+  icon: string;
+  routerLink: string;
+  content: string;
+}
