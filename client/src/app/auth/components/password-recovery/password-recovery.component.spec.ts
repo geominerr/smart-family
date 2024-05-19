2c20edf99915c04bed917d3b21647d5b3a4a8873
Maksym Voloshyn <mv.webdew@gmail.com>
2024-05-19 16:55:25 +0300

    test: add auth components tests

--- client/src/app/auth/components/password-recovery/password-recovery.component.spec.ts
+++ client/src/app/auth/components/password-recovery/password-recovery.component.spec.ts
@@ -1,26 +1,69 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { NgZone } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 
+import { AuthService } from '../../services/auth.service';
 import { PasswordRecoveryComponent } from './password-recovery.component';
 
 describe('PasswordRecoveryComponent', () => {
   let component: PasswordRecoveryComponent;
   let fixture: ComponentFixture<PasswordRecoveryComponent>;
+  let ngZone: NgZone;
+  const mockAuthService = {
+    resetPassword: () => of({}),
+  };
+  const submitButtonSelector = '.password-recovery-form__submit-button';
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BrowserAnimationsModule, PasswordRecoveryComponent],
-      providers: [provideRouter([]), provideHttpClient()],
+      imports: [
+        NoopAnimationsModule,
+        PasswordRecoveryComponent,
+        RouterTestingModule,
+      ],
+      providers: [{ provide: AuthService, useValue: mockAuthService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PasswordRecoveryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    ngZone = TestBed.inject(NgZone);
   });
 
-  test('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show error message if the field is empty', () => {
+    const matErrorSelector = 'mat-error';
+    const submitButton = fixture.debugElement.query(
+      By.css(submitButtonSelector)
+    );
+    const invalidFormData = {
+      email: '',
+    };
+    component.passwordResetForm.setValue(invalidFormData);
+
+    submitButton.triggerEventHandler('click', {});
+    fixture.detectChanges();
+
+    const matErrors = fixture.debugElement.queryAll(By.css(matErrorSelector));
+    expect(matErrors.length).toBe(1);
+  });
+
+  it('should call the resetPassword after the click if the form is valid', () => {
+    const resetPasswordSpyon = jest.spyOn(mockAuthService, 'resetPassword');
+    const submitButton = fixture.debugElement.query(
+      By.css(submitButtonSelector)
+    );
+    const validFormData = {
+      email: 'valid@email.com',
+    };
+    component.passwordResetForm.setValue(validFormData);
+
+    ngZone.run(() => {
+      submitButton.triggerEventHandler('click', {});
+
+      expect(resetPasswordSpyon).toHaveBeenCalledWith(validFormData);
+    });
   });
 });
