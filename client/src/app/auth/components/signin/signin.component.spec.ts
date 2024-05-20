4dbf884828b0435f3b6787ffc176be24afde09f5
Maksym Voloshyn <mv.webdew@gmail.com>
2024-05-20 11:45:31 +0300

    test: add auth service tests

--- client/src/app/auth/components/signin/signin.component.spec.ts
+++ client/src/app/auth/components/signin/signin.component.spec.ts
@@ -2,7 +2,6 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgZone } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
-import { ActivatedRoute, Router, provideRouter } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
 import { of } from 'rxjs';
