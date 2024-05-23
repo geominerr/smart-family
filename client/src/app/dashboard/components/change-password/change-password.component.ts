c6df5a9feb3a8f6804cde727e173203292e0e74c
Maksym Voloshyn <mv.webdew@gmail.com>
2024-05-23 19:36:48 +0300

    feat: update change password

--- client/src/app/dashboard/components/change-password/change-password.component.ts
+++ client/src/app/dashboard/components/change-password/change-password.component.ts
@@ -1,7 +1,13 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { AsyncPipe, NgIf } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, Subscription, filter } from 'rxjs';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -11,6 +17,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { Store } from '@ngrx/store';
 import { UserActions } from '@app/store/user/user.actions';
 import { selectUserId } from '@app/store/user/user.selectors';
+import { selectUpdateUserStatus } from '@app/store/status/status.selector';
 
 import { passwordValidator } from '@app/shared/validators/password.validator';
 import { getPaswordCompareValidator } from '@app/shared/validators/password-compare.validator';
@@ -33,7 +40,7 @@ type TControlName = 'oldPassword' | 'newPassword' | 'confirmPassword';
     MatFormFieldModule,
   ],
 })
-export class ChangePasswordComponent implements OnInit {
+export class ChangePasswordComponent implements OnInit, OnDestroy {
   userId$: Observable<string | undefined> | undefined;
 
   sumbitted: boolean = false;
@@ -46,15 +53,23 @@ export class ChangePasswordComponent implements OnInit {
     confirmPassword: false,
   };
 
-  constructor(private store: Store, private fb: FormBuilder) {}
+  private subcription: Subscription | undefined;
+
+  private validationError = { password: 'Incorrect old password' };
+
+  constructor(
+    private store: Store,
+    private fb: FormBuilder,
+    private ref: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.userId$ = this.store.select(selectUserId);
 
     this.form = this.fb.group({
-      oldPassword: ['', passwordValidator],
-      newPassword: ['', passwordValidator],
-      confirmPassword: ['', passwordValidator],
+      oldPassword: ['', [passwordValidator]],
+      newPassword: ['', [passwordValidator]],
+      confirmPassword: ['', [passwordValidator]],
     });
 
     this.form.addValidators(
@@ -63,6 +78,23 @@ export class ChangePasswordComponent implements OnInit {
         this.form.controls['confirmPassword']
       )
     );
+
+    this.subcription = this.store
+      .select(selectUpdateUserStatus)
+      .pipe(filter((status) => !!status))
+      .subscribe((status) => {
+        if (status?.error) {
+          const error = status.error as { statusCode: number };
+
+          if (error?.statusCode === 403) {
+            this.showIncorrectPasswordError();
+          }
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subcription?.unsubscribe();
   }
 
   getErrorMessage(control: TControlName): string {
@@ -77,9 +109,12 @@ export class ChangePasswordComponent implements OnInit {
   }
 
   submit(userId: string): void {
+    if (!this.sumbitted) {
+      this.sumbitted = true;
+    }
+
     if (this.form.invalid) {
       this.form.markAllAsTouched();
-      this.sumbitted = true;
 
       return;
     }
@@ -92,5 +127,13 @@ export class ChangePasswordComponent implements OnInit {
         dto: { oldPassword, newPassword },
       })
     );
+  }
+
+  private showIncorrectPasswordError() {
+    const { oldPassword } = this.form.controls;
+    oldPassword.setErrors(this.validationError);
+    oldPassword.markAsTouched();
+
+    this.ref.markForCheck();
   }
 }
