a3855a394470853c5fef0de1500cc7388b69115f
Maksym Voloshyn <mv.webdew@gmail.com>
2024-07-08 22:16:45 +0300

    fix: change api url

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'api/url',
-  urlGoogleAuth: 'api/auth/google',
+  apiUrl: 'https://api.smart-family.online',
+  urlGoogleAuth: 'https://api.smart-family.online/auth/google',
 };
