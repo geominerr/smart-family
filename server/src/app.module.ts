0d5d9e2afa683cb17f4341bfb4c328cd615c845c
Maksym Voloshyn <mv.webdew@gmail.com>
2024-07-08 15:50:04 +0300

    fix: remove filter

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 
 import { appConfig, jwtConfig, googleConfig } from '@app/config/config.config';
 import { CoreModule } from '@app/core/core.module';
 import { JwtAuthGuard } from '@app/core/auth/guards/jwt.guard';
 import { PrismaService } from '@app/prisma/prisma.service';
 import { PrismaModule } from '@app/prisma/prisma.module';
-import { ExceptionFilterModule } from '@app/exception-filter/exeption-filter.module';
-import { CustomFilter } from '@app/exception-filter/custom-filter.filter';
+
 import { LoggingInterceptor } from '@app/logger/logger.interceptor';
 import { LoggerModule } from '@app/logger/logger.module';
 
@@ -18,7 +17,6 @@ import { LoggerModule } from '@app/logger/logger.module';
     CoreModule,
     PrismaModule,
     LoggerModule,
-    ExceptionFilterModule,
   ],
   controllers: [],
   providers: [
@@ -27,7 +25,6 @@ import { LoggerModule } from '@app/logger/logger.module';
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
     },
-    { provide: APP_FILTER, useClass: CustomFilter },
     { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
   ],
 })
