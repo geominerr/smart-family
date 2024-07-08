48c5a7d3bdb4e094d0ab768cd621c2f75764e882
Maksym Voloshyn <mv.webdew@gmail.com>
2024-07-08 23:39:03 +0300

    fix: fix progress bar delay

--- client/src/app/shared/components/status-bar/status-bar.component.ts
+++ client/src/app/shared/components/status-bar/status-bar.component.ts
@@ -2,7 +2,7 @@ import { AsyncPipe, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { RequestStateService } from '@app/shared/services/request-state.service';
-import { Observable, delay } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   standalone: true,
@@ -18,8 +18,6 @@ export class StatusBarComponent implements OnInit {
   constructor(private requestStateService: RequestStateService) {}
 
   ngOnInit(): void {
-    this.isPendingResponce$ = this.requestStateService
-      .getLoadingState()
-      .pipe(delay(0));
+    this.isPendingResponce$ = this.requestStateService.getLoadingState();
   }
 }
