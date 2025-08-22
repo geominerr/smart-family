fd25260531b89fcb27391a56916689cdc4330cd9
Maksym Voloshyn <mv.webdew@gmail.com>
2025-08-22 23:05:39 +0300

    chore: update env

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.smart-family.online',
-  urlGoogleAuth: 'https://api.smart-family.online/auth/google',
+  apiUrl: 'https://api.smart-family.space',
+  urlGoogleAuth: 'https://api.smart-family.space/auth/google',
 };
